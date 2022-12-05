c8511257b15b615bcfbc08b2e0feac0d58c3c8bc
kevinnail <fumalicious@gmail.com>
2022-12-05 14:50:11 -0800

    oauth working, /login loads github auth page, added github controller/ service, updated app.js

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,11 +1,18 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users3;
 
-CREATE TABLE users (
+CREATE TABLE users3 (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email VARCHAR,
   password_hash VARCHAR NOT NULL,
   first_name VARCHAR NOT NULL,
   last_name VARCHAR NOT NULL
+);
+
+CREATE TABLE github_users(
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  login TEXT NOT NULL,
+  email TEXT, 
+  avatar TEXT
 );
